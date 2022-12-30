41b05d39b887946e0f3f056514a05ab837150d25
Taehillah <ishmaelmafole@gmail.com>
2022-12-31 00:22:22 +0200

    Updates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var sign = document.getElementById('signup');
 var former = document.getElementById('forms');
 
-sign.addEventListener(sign, 'click', function(){
+former.addEventListener('click', function(){
 alert("Clicked  " + sign);
-}
-)
+}, false
+);
