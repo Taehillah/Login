157980c4dfbdfcadf600d4eb25f052762be331bc
fedora <ishmaelmafole@gmail.com>
2023-01-01 11:22:38 +0200

    Updates

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ var signup = document.getElementById('signup');
 var signin= document.getElementById('signin');
 
 signin.addEventListener('click', function(){
-signup.style.color ="grey";
+    signup.style.color ="grey";
+    signin.style.color ="white";
+    signup.style.borderBottomStyle="none";
+    signin.style.borderBottomStyle="solid";
+    signin.style.borderBottomWidth="1px";
+    signin.style.borderBottomColor="rgba(200, 134, 156, 0.851)"
 }, false
 );
+
+signup.addEventListener('click', function(){
+    signin.style.color ="grey";
+    signup.style.color ="white";
+    signin.style.borderBottomStyle="none";
+    signup.style.borderBottomStyle="solid";
+    signup.style.borderBottomWidth="1px";
+    signup.style.borderBottomColor="rgba(200, 134, 156, 0.851)"
+})
