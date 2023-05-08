a81168928dcbdc30dce5582bdc250e18984068f3
Taehillah <ishmaelmafole@gmail.com>
2023-05-08 17:59:53 +0200

    Updates

---
+++ receive.php
@@ -0,0 +1,21 @@
+<?php
+$usernameInput = $_POST["fusername"];
+$passwordInput1 = $_POST["fpassword"];
+$password2Input = $_POST["frepeat_password"];
+
+if($usernameInput != 1){
+echo "<p>hi</p>";
+} ;
+
+
+
+
+
+
+
+
+
+
+
+
+?>
